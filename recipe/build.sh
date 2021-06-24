06a324ac7c89f2d416880373fc1d928bdbc4c10c
Allan Leal <allanmulin@gmail.com>
2021-06-24 10:02:56 +0200

    Minor fix/cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,16 +20,3 @@ cmake -S . -B build \
 
 # Build and install Reaktoro and the dependencies above in $PREFIX
 cmake --build build --target install --parallel
-
-# mkdir build
-# cd build
-# cmake -DREAKTORO_BUILD_PYTHON=ON \
-#       -DREAKTORO_DEPS_EXTRA_BUILD_ARGS="-DCMAKE_VERBOSE_MAKEFILE=ON" \
-#       -DCMAKE_BUILD_TYPE=Release \
-#       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-#       -DCMAKE_INSTALL_LIBDIR=lib \
-#       -DCMAKE_VERBOSE_MAKEFILE=ON \
-#       -DPYTHON_EXECUTABLE=$PYTHON \
-#       ..
-# make VERBOSE=1 -j${CPU_COUNT}
-# make install

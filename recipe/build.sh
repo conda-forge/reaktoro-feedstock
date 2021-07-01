63e0e010348436b54e367ae3f78c523afa1a025e
Allan Leal <allanmulin@gmail.com>
2021-07-02 00:36:23 +0200

    Using parallel jobs in unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Avoid errors on the server such as:
-#  - Error: virtual memory exhausted: Cannot allocate memory
-#  - Error: Exit code 137
-# due to many parallel jobs consuming all available memory
-JOBS=$((CPU_COUNT - 1))
-
-echo "Using $JOBS parallel jobs out of $CPU_COUNT available to build Reaktoro and its dependencies."
-
 # Configure the build of the dependencies in the deps directory
 cmake -S deps -B deps/build \
     -DCMAKE_BUILD_TYPE=Release \
@@ -15,7 +7,7 @@ cmake -S deps -B deps/build \
     -DPYTHON_EXECUTABLE=$PYTHON
 
 # Build the dependencies in the deps directory
-cmake --build deps/build --parallel $JOBS
+cmake --build deps/build --parallel
 
 # Configure the build of Reaktoro
 cmake -S . -B build \
@@ -27,4 +19,4 @@ cmake -S . -B build \
     -DREAKTORO_BUILD_PYTHON=ON
 
 # Build and install Reaktoro and the dependencies above in $PREFIX
-cmake --build build --target install --parallel $JOBS
+cmake --build build --target install --parallel

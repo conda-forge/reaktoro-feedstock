d054b83bc531c6badc2e099ef4da1ccf14524c6a
Allan Leal <allanmulin@gmail.com>
2023-09-04 10:03:12 +0200

    Enable build of tests and their execution

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,11 @@ cmake -GNinja ..                              ^
     -DCMAKE_BUILD_TYPE=Release                ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
     -DCMAKE_INCLUDE_PATH=%LIBRARY_INC%        ^
-    -DREAKTORO_BUILD_TESTS=OFF                ^
     -DREAKTORO_PYTHON_INSTALL_PREFIX=%PREFIX% ^
     -DPYTHON_EXECUTABLE=%PYTHON%
 
-@REM Build and install Reaktoro in %LIBRARY_PREFIX%
+@REM Build and test Reaktoro
+ninja tests
+
+@REM Install Reaktoro in %LIBRARY_PREFIX%
 ninja install

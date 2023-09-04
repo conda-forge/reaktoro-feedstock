9d3fa0b6329e31dbf5bf3a3b7f3f6ee0264acc4e
Allan Leal <allanmulin@gmail.com>
2023-09-04 17:58:14 +0200

    Skip ninja tests if cross-compiling (e.g., osx-arm64)

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,10 @@ cmake -GNinja .. ${CMAKE_ARGS}  \
     -DPYTHON_EXECUTABLE=$PYTHON \
     -DCMAKE_CXX_FLAGS="$(if [ $(uname) == 'Linux' ]; then echo '-fuse-ld=lld'; else echo ''; fi)"
 
-# Build and test Reaktoro
-ninja tests
+# Build and test Reaktoro (except when cross-compiling, e.g., osx-arm64 builds)
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  ninja tests
+fi
 
 # Install Reaktoro in $PREFIX
 ninja install
